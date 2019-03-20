2ed175225cf63b3930fcc5c3748f2bf3ccdde6fa
lala341 <li.forero@uniandes.edu.co>
2019-03-19 22:18:56 -0500

    correccionp

--- productos/views.py
+++ productos/views.py
@@ -44,7 +44,7 @@ def ProductoUpdate(request,pk):
         if form.is_valid():
             form.save()
             messages.add_message(request, messages.SUCCESS, 'Producto update successful')
-            return redirect('producto:ProductoList')
+            return HttpResponseRedirect(reverse('productoUpdate/'+pk))
 
     context = {
         'form': form,
