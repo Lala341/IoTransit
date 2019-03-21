b2c8600aea5b2343a03a12cc3ef0263bc36dcb94
lala341 <li.forero@uniandes.edu.co>
2019-03-21 16:53:19 -0500

    camcio dis

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('productos.urls')),
+    path('', include('productos.urls'),name='base_layout'),
     path('', include('pwa.urls')),
 ]
