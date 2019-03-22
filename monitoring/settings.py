f52f713c1c999a988a07508fa7772fd35b72c25b
lala341 <li.forero@uniandes.edu.co>
2019-03-21 19:10:48 -0500

    camcio dis

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -16,7 +16,6 @@ from django.db import connections
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PWA_SERVICE_WORKER_PATH = os.path.join(PROJECT_ROOT, 'static/js','serviceworker.js')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -136,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PWA_SERVICE_WORKER_PATH = os.path.join(PROJECT_ROOT, 'static/js','serviceworker.js')
+
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
